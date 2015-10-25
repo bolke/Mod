91a7f100f80667d05bb48ef85acefa7f4a483083
bolke <aniko@rtl4.nl>
2015-10-25 13:00:25 +0100

    Created a comparable pipe, comparers each input with each other item.

---
+++ ModElements/Modules/ComparablePipe.cs
@@ -0,0 +1,32 @@
+﻿using ModBase.Modules;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModElements.Modules
+{
+  public class ComparablePipe<T>: Pipe<T>{
+    public override bool Push(T element)
+    {
+      lock(Padlock)
+      {
+        IComparable compare = element as IComparable;
+        if(compare != null)
+        {
+          for(int i = 0; i < Count; i++)
+          {
+            if(compare.CompareTo(Data[i]) <= 0)
+            {
+              Insert(i, element);
+              return true;
+            }
+          }
+        }
+        base.Push(element);
+        return true;
+      }
+    }
+  }
+}
