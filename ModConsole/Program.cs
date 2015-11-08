259ba46c315948a6b90e3de1208e1d7cf882c32f
bolke <aniko@rtl4.nl>
2015-11-08 21:44:16 +0100

    Removed streampipe, thought of new ways to mix streams into the dance.

--- ModConsole/Program.cs
+++ ModConsole/Program.cs
@@ -14,28 +14,6 @@ namespace ModConsole
   {
     static void Main(string[] args)
     {
-      SerialPort comPort = new SerialPort("COM11");
-
-      comPort.BaudRate = 9600;
-      comPort.StopBits = StopBits.One;
-      comPort.DataBits = 8;
-
-      comPort.ReadTimeout = 10;
-      comPort.Open();
-
-      StreamPipe<byte[]> knutty = new StreamPipe<byte[]>();
-      knutty.Initialize();
-
-      knutty.Stream = comPort.BaseStream;
-
-      knutty.Push("0123456789".ToCharArray() as byte[]);
-
-      while(true)
-      {
-        Console.WriteLine(knutty.Pop());
-        Thread.Sleep(1000);
-      }
-
       ModConfigSectionReader loader = new ModConfigSectionReader();
       loader.LoadAndRun();
       Console.ReadLine();
