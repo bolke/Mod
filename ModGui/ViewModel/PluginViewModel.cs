2698c1537c35e089ab86725a13ed37129a6175b1
bolke <aniko@rtl4.nl>
2015-11-23 00:28:52 +0100

    Go.

---
+++ ModGui/ViewModel/PluginViewModel.cs
@@ -0,0 +1,44 @@
+﻿using Mod.Configuration.Plugins;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModGui.ViewModel
+{
+  public class PluginViewModelCollection
+  {
+    public ObservableCollection<PluginViewModel> plugins = new ObservableCollection<PluginViewModel>();
+  }
+
+  public class PluginViewModel
+  {
+    protected PluginConfig config
+    {
+      get;
+      set;
+    }
+
+    public string Name
+    {
+      get { return IsLoaded?config.Assembly.FullName:""; }      
+    }
+
+    public string File
+    {
+      get { return config.File; }
+    }
+
+    public string Path
+    {
+      get { return config.Path; }
+    }
+
+    public bool IsLoaded
+    {
+      get { return config.Assembly != null; }
+    }
+  }
+}
