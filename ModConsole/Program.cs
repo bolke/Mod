64365e5f3564e5a8f8bf5457f67662f4b2f6467f
bolke <aniko@rtl4.nl>
2015-11-27 01:22:04 +0100

    Recreated test program.

--- ModConsole/Program.cs
+++ ModConsole/Program.cs
@@ -1,9 +1,12 @@
-﻿using Mod.Configuration.Section;
+﻿using Mod.Configuration.Modules;
+using Mod.Configuration.Section;
+using Mod.Interfaces;
 using Mod.Modules;
 using Mod.Modules.Combinations;
 using Mod.Modules.EndPoints;
 using Mod.Modules.Lines;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.IO.Ports;
@@ -20,24 +23,23 @@ namespace ModConsole
   {
     static void Main(string[] args)
     {
-      TcpClient client = new TcpClient();
-      client.Connect("192.168.178.54", 54000);
-
       Pipe<String> pis = new Pipe<String>();
       Pipe<String> pis2 = new Pipe<String>();
 
       SerialPort sp2 = new SerialPort("COM12");
+      SerialPort sp1 = new SerialPort("COM11");
 
       PipeStream ps = new PipeStream();
       PipeStream ps2 = new PipeStream();
 
+      sp1.Open();
       sp2.Open();
       
       ps.AutoOpen = true;
       ps2.AutoOpen = true;
 
       ps2.Stream = sp2.BaseStream;
-      ps.Stream = client.GetStream();
+      ps.Stream = sp1.BaseStream;//.GetStream();
 
       pis.BasePipe = ps;
       pis2.BasePipe = ps2;
@@ -48,17 +50,28 @@ namespace ModConsole
       ps2.Initialize();
       ps.Initialize();
 
-      int i = 0;
- 
-      while (true)
+      /*while (true)
       {
         ps2.PushObject(ps.PopObject());
         ps.PushObject(ps2.PopObject());
         Thread.Sleep(100);
-      }
+      }*/
 
-      //ModConfigSectionReader loader = new ModConfigSectionReader();
-      //loader.LoadAndRun();      
+      ModConfigSectionReader loader = new ModConfigSectionReader();
+      loader.Load();
+
+      for(int i = 0; i < loader.ModuleInstances.Count;i++ )
+      {
+        ModuleConfig module = loader.ModuleInstances.ElementAt(i).Value;
+
+        if(module != null)
+        {
+          if((module.Instance as IRunnable) != null)
+          {
+            (module.Instance as IRunnable).Start();
+          }
+        }
+      }
       Console.ReadLine();
     }
   }
